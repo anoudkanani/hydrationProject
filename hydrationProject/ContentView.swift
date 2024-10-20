bce1797f77e2248ded6b5ef4a3a4834556d2ebf3
anoudkanani <anoudnasserkanani@gmail.com>
2024-10-20 14:39:33 +0300

    Initial Commit

---
+++ hydrationProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hydrationProject
+//
+//  Created by alanoud on 17/04/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hydrationProject/hydrationProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  hydrationProjectApp.swift
+//  hydrationProject
+//
+//  Created by alanoud on 17/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct hydrationProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
